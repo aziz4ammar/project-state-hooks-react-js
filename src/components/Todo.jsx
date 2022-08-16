f022b4fe2d6dd605e7c93a6af54a27aeb8281e9c
mohamed aziz ammar <azizammar67@gmail.com>
2022-08-16 10:01:41 +0100

    adding demo version

---
+++ src/components/Todo.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const Todo = () => {
+
+    const [tasks, setTasks] = useState(math.random())
+
+
+  return (
+    <div>
+        <h1></h1>
+        <button>Delete</button>
+        <button>Complete</button>
+    </div>
+  )
+}
+
+export default Todo
